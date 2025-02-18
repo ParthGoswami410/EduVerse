aa11a3181177f35d4317e47a559d909975e07cd2
Parth <parth9210328@gmail.com>
2025-02-18 22:26:37 +0530

    Added

--- student/urls.py
+++ student/urls.py
@@ -3,5 +3,9 @@ from django.urls import path,include
 from .import views
 
 urlpatterns = [
-  
+  path("",views.student_list,name='student_list'),
+  path("add/",views.add_Student,name='add_Student'),
+  path('students/<str:slug>/',views.view_student,name='view_student')
+
+
 ]
